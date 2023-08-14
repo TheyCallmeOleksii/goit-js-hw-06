7e65ffae4654e375aad5a7fb5d0739a9507871a5
TheyCallmeOleksii <DionodeKor@gmail.com>
2023-08-14 18:35:40 +0300

    Summary 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  if (!email || !password) {
+    alert("All fields must be filled");
+    return;
+  }
+
+  const user = {
+    email,
+    password,
+  };
+
+  console.log(user);
+
+  loginForm.reset();
+});
